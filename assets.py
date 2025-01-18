af22fdf18f605c38770e6f045b1c7dc211b0eff3
Noah TerBest <noahterbest@gmail.com>
2025-01-17 20:49:57 -0700

    2025.1.0.1

--- assets.py
+++ assets.py
@@ -1,6 +1,30 @@
+import pygame
+
 def doShit():
     return "Doing Shit"
 
 class Ant:
+    def __init__(self, x, y, screen_width, screen_height):
+        self.image = pygame.transform.scale(pygame.image.load('assets/ant.png').convert_alpha(), (32, 32))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = 20
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+
+    def move(self, dx, dy):
+        new_x = self.rect.x + dx * self.speed
+        new_y = self.rect.y + dy * self.speed
+
+        #Check if new position is within screen boundaries
+        if 0 <= new_x <= self.screen_width - self.rect.width:
+            self.rect.x = new_x
+        if 0 <= new_y <= self.screen_height - self.rect.height:
+            self.rect.y = new_y
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
     def speak(self):
         return "Doing Ant"
--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import pygame
 from assets import Ant
 
-version = "2025.1.0"
+version = "2025.1.0.1"
 print(" ")
 print("Game version: " + version)
 
@@ -15,6 +15,15 @@ class GameEngine:
         self.clock = pygame.time.Clock()
         self.running = True
 
+        #Load background
+        self.background = pygame.image.load("assets/background.png")
+        self.background = pygame.transform.scale(self.background, (width, height))
+        self.original_background = self.background.copy()
+        self.original_width, self.original_height = self.screen.get_size()
+
+        #Load Character
+        self.character = Ant(400, 300, width, height)
+
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -25,15 +34,19 @@ class GameEngine:
                     print(Ant.speak(self))
                     pass
                 if event.key == pygame.K_UP:
+                    self.character.move(0, -1)
                     print("Up pressed.")
                     pass
                 if event.key == pygame.K_DOWN:
+                    self.character.move(0,1)
                     print("Down pressed.")
                     pass
                 if event.key == pygame.K_LEFT:
+                    self.character.move(-1,0)
                     print("Left pressed.")
                     pass
                 if event.key == pygame.K_RIGHT:
+                    self.character.move(1,0)
                     print("Right pressed.")
                     pass
 
@@ -43,7 +56,8 @@ class GameEngine:
 
     def draw(self):
         # Handles rendering. Here, you'd draw sprites, backgrounds, etc.
-        self.screen.fill((0, 0, 0)) #Black background
+        self.screen.blit(self.background, (0, 0))
+        self.character.draw(self.screen)
         pass
 
     def run(self):
